f9a85dbb84dfbbf6962a9e11e695d2e7d9aca9d8
dkhor2003 <khordylan2003@gmail.com>
2023-07-31 22:33:02 -0500

    Project 1 and Mini Contest 1

--- Pac-Man_Search/Mini_Contest/minicontest1/myAgents.py
+++ Pac-Man_Search/Mini_Contest/minicontest1/myAgents.py
@@ -29,6 +29,7 @@ but when you're ready to test your own agent, replace it with MyAgent
 pacmen_num = 0
 pacmen_status = {}
 pacmen_positions = []
+pacmen_target = []
 
 def createAgents(num_pacmen, agent='MyAgent'): #ClosestDotAgent
     global pacmen_num
@@ -48,23 +49,19 @@ class MyAgent(Agent):
         """
 
         "*** YOUR CODE HERE"
-        global pacmen_status, pacmen_positions, pacmen_num
-        pacmen_status = {agentIndex: [None, []] for agentIndex in range(pacmen_num)}
+        global pacmen_status, pacmen_positions, pacmen_num, pacmen_target
+        pacmen_status = {agentIndex: [] for agentIndex in range(pacmen_num)} 
         pacmen_positions = [(-1, -1) for agentIndex in range(pacmen_num)]
+        pacmen_target = [None for agentIndex in range(pacmen_num)]
         
     def getAction(self, gameState):
-        global pacmen_status, pacmen_positions
+        global pacmen_status, pacmen_positions, pacmen_target
         pacmen_positions[self.index] = gameState.getPacmanPosition(self.index)
         problem = AnyFoodSearchProblem(gameState, self.index)
         food_grid = gameState.getFood()
         food_list = food_grid.asList()
-        if pacmen_status[self.index][0] not in food_list:
-            pacmen_status[self.index][1] = []
-    
-        rand = random.randint(0, len(food_list)-1)
-        random_food = food_list[rand]
-        if pacmen_status[self.index][0] != random_food and len(pacmen_status[self.index][1]) == 0:
-            pacmen_status[self.index][0] = random_food
+
+        if len(pacmen_status[self.index]) == 0:
             close_to_self = []
             self_position = pacmen_positions[self.index]
             for pacman_index in range(pacmen_num):
@@ -86,21 +83,115 @@ class MyAgent(Agent):
                     if self_dist < min(computeDistanceToFood(nearby_pacmen, food)) and self_dist < min_dist_food[0]:
                         min_dist_food = [self_dist, food]
                 if min_dist_food[1] == None:
-                    pacmen_status[self.index][1] = [Directions.STOP]
+                    pacmen_status[self.index] = [Directions.STOP]
+                    pacmen_target[self.index] = None
                 else:
                     goal = min_dist_food[1]
                     problem = PositionSearchProblem(gameState, agentIndex=self.index, goal=goal, warn=False, visualize=False)
-                    pacmen_status[self.index][1] = search.astar(problem, heuristic=manhattanHeuristic)
+                    pacmen_status[self.index], pacmen_target[self.index] = astar(problem)
                 
             else:
-                pacmen_status[self.index][1] = search.bfs(problem)
+                pacmen_status[self.index], pacmen_target[self.index] = bfs(problem)
                 
-        elif pacmen_status[self.index][0] == random_food and len(pacmen_status[self.index][1]) == 0:
-            pacmen_status[self.index][1] = search.bfs(problem)
+        elif pacmen_target[self.index] not in food_list:
+            pacmen_status[self.index], pacmen_target[self.index] = bfs(problem)
+            
+#         elif pacmen_target.count(pacmen_target[self.index]) > 1 and pacmen_target[self.index]:
+#             same_targets = [i for i in range(len(pacmen_target)) if pacmen_target[i] == pacmen_target[self.index] and i != self.index]
+#             for pacman_index in same_targets:
+#                 if len(pacmen_status[self.index]) > len(pacmen_status[pacman_index]):
+#                     if len(food_list) > 1:
+#                         tempGameState = gameState.deepCopy()
+#                         x, y = pacmen_target[self.index]
+#                         tempGameState.data.food[x][y] = False
+#                         newProblem = AnyFoodSearchProblem(tempGameState, self.index)
+#                         pacmen_status[self.index], pacmen_target[self.index] = bfs(newProblem)
+#                     else:
+#                         pacmen_status[self.index] = [Directions.STOP for i in range(len(pacmen_status[pacman_index]))]
+#                         pacmen_target[self.index] = None
+                        
+#                     break
 
-        return pacmen_status[self.index][1].pop(0)
+        return pacmen_status[self.index].pop(0)
 
+    
+def bfs(problem):
+    """Search the shallowest nodes in the search tree first."""
+    "*** YOUR CODE HERE ***"
+    start = problem.getStartState()
+    moves = []
+    explored = []
+    queue = util.Queue()
+    queue.push((start, moves))
+    
+    while not queue.isEmpty():
+        item = queue.pop()
+        state = item[0]
+        moves = item[1]
         
+        if state not in explored:
+            explored.append(state)
+            
+        if problem.isGoalState(state):
+            return (moves, state)
+        
+        for nextMoves in problem.getSuccessors(state):
+            nextState = nextMoves[0]
+            states_in_queue = [s[0] for s in queue.list]
+            if nextState not in explored and nextState not in states_in_queue:
+                action = moves + [nextMoves[1]]
+                queue.push((nextState, action))
+    return ([], None)
+
+
+def astar(problem, heuristic=manhattanHeuristic):
+    """Search the node that has the lowest combined cost and heuristic first."""
+    "*** YOUR CODE HERE ***"
+    start = problem.getStartState()
+    explored = []
+    moves = []
+    heap = util.PriorityQueue()
+    heap.push((start, moves), 0)
+    
+    while not heap.isEmpty():
+        item = heap.pop()
+        state = item[0]
+        moves = item[1]
+            
+        if problem.isGoalState(state):
+            return (moves, state)
+        
+        if state not in explored:
+            explored.append(state)
+            
+            nextMoves = problem.getSuccessors(state)
+        
+            for move in nextMoves:
+                nextState = move[0]
+
+                if nextState in explored:
+                    continue
+                
+                action = move[1]
+                cost = move[2]
+            
+                new_move = moves + [action]
+                new_cost = problem.getCostOfActions(moves) + cost + heuristic(nextState, problem)
+
+                if nextState in getHeapStates(heap) and getPriority(heap, nextState) < new_cost:
+                    continue
+                
+                heap.update((nextState, new_move), new_cost)
+    return ([], None)
+
+def getPriority(priority_queue, state):
+    for items in priority_queue.heap:
+        if items[2][0] == state:
+            return items[0]  
+    return None
+    
+def getHeapStates(priority_queue):
+    return [states[2][0] for states in priority_queue.heap]
 
 """
 Put any other SearchProblems or search methods below. You may also import classes/methods in
@@ -145,13 +236,12 @@ class AnyFoodSearchProblem(PositionSearchProblem):
         "Stores information from the gameState.  You don't need to change this."
         # Store the food for later reference
         self.food = gameState.getFood()
-
         # Store info for the PositionSearchProblem (no need to change this)
         self.walls = gameState.getWalls()
         self.startState = gameState.getPacmanPosition(agentIndex)
         self.costFn = lambda x: 1
         self._visited, self._visitedlist, self._expanded = {}, [], 0 # DO NOT CHANGE
-
+        
     def isGoalState(self, state):
         """
         The state is Pacman's position. Fill this in with a goal test that will
