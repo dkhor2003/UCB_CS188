6be282e48df9a152ddda4d82ae7e6dc508a8dc66
dkhor2003 <khordylan2003@gmail.com>
2023-08-04 13:46:16 -0500

    Updated Mini Contest 2

--- Multi-Agent_Search/minicontest2/myTeam.py
+++ Multi-Agent_Search/minicontest2/myTeam.py
@@ -46,51 +46,6 @@ def createTeam(firstIndex, secondIndex, isRed, first = 'OffensiveReflexAgent', s
 # Agents #
 ##########
 
-# class DummyAgent(CaptureAgent):
-#     """
-#       A Dummy agent to serve as an example of the necessary agent structure.
-#       You should look at baselineTeam.py for more details about how to
-#       create an agent as this is the bare minimum.
-#       """
-
-#     def registerInitialState(self, gameState):
-#         """
-#         This method handles the initial setup of the
-#         agent to populate useful fields (such as what team
-#         we're on).
-
-#         A distanceCalculator instance caches the maze distances
-#         between each pair of positions, so your agents can use:
-#         self.distancer.getDistance(p1, p2)
-
-#         IMPORTANT: This method may run for at most 15 seconds.
-#         """
-
-#         '''
-#         Make sure you do not delete the following line. If you would like to
-#         use Manhattan distances instead of maze distances in order to save
-#         on initialization time, please take a look at
-#         CaptureAgent.registerInitialState in captureAgents.py.
-#         '''
-#         CaptureAgent.registerInitialState(self, gameState)
-
-#         '''
-#         Your initialization code goes here, if you need any.
-#         '''
-
-
-#     def chooseAction(self, gameState):
-#         """
-#         Picks among actions randomly.
-#         """
-#         actions = gameState.getLegalActions(self.index)
-
-#         '''
-#         You should change this in your own agent.
-#         '''
-
-#         return random.choice(actions)
-
 class ReflexCaptureAgent(CaptureAgent):
     """
     A base class for reflex agents that chooses score-maximizing actions
@@ -107,8 +62,6 @@ class ReflexCaptureAgent(CaptureAgent):
             if not gameState.hasWall(middle, y):
                 self.middleColumn.append((middle, y))
                 
-        self.initialFoodCount = len(self.getFood(gameState).asList())
-        self.teamIndices = self.getTeam(gameState)
 
     def chooseAction(self, gameState):
         """
@@ -230,6 +183,7 @@ class OffensiveReflexAgent(ReflexCaptureAgent):
     def getWeights(self, gameState, action):
         return {"food_eval": -1, "mid_eval": -2, "unscared_ghost_eval": -1, "scared_ghost_eval": 100, "successor_score": 100, "capsule_dist": -100}
 
+    
 class DefensiveReflexAgent(ReflexCaptureAgent):
     """
     A reflex agent that keeps its side Pacman-free. Again,
